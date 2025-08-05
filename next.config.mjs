f0397ab950c91ca9872b92da122200977996d732
Snandi891 <nitunandi1985@gmail.com>
2025-08-04 22:21:34 -0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Add this line to skip ESLint on build
+  },
 };
 
 export default nextConfig;
